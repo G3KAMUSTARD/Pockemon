0e082bbd91550f1d062f105d20be6cc24d17cc0b
G3KAMUSTARD <archibald0919@mail.ru>
2024-09-13 14:36:33 +0400

    v 1.0
    add base class pockemon

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pockemonnaumov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+    class Pokemon
+    {
+        public int HP { get; set; }
+        public int attack { get; set; }
+        public int defense { get; set; }
+
+        public int special_attack { get; set; }
+        public int special_defense { get; set; }
+        public int speed { get; set; }
+    }
+    
+}
