90ad64c0dd25f706992938a4fb54b9180e8b3aed
G3KAMUSTARD <archibald0919@mail.ru>
2024-09-13 14:38:28 +0400

    v 1.1
    add gible and hp,attack parameters

--- Program.cs
+++ Program.cs
@@ -23,5 +23,18 @@ namespace pockemonnaumov
         public int special_defense { get; set; }
         public int speed { get; set; }
     }
-    
+    class Gible
+    {
+        Pokemon pokemon = new Pokemon();
+
+        public void statics()
+        {
+            pokemon.HP = 400;
+            pokemon.attack = 500;
+            pokemon.defense = 0;
+            pokemon.special_attack = 0;
+            pokemon.special_defense = 0;
+            pokemon.speed = 0;
+        }
+    }
 }
